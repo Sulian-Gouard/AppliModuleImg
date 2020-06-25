fcdd3f2f8efd93e0804d5a9426be6e4334edba13
Sulian-Gouard <suliangouardlanoe@gmail.com>
2020-06-25 15:44:32 +0200

    to pull

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ require_once 'my-config.php';
 </head>
 
 <body>
-    
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
