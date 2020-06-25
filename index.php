aff460443443af1328524dcf17dd7f3f9bc13757
Sulian-Gouard <suliangouardlanoe@gmail.com>
2020-06-25 14:09:53 +0200

    base base

--- index.php
+++ index.php
@@ -83,7 +83,6 @@ if (!empty($_POST)) {
         $message = 'Veuillez uploader une image valide (Taille, Type ... ) !';
     }
 }
-var_dump($_FILES);
 
 ?>
 
@@ -99,11 +98,11 @@ var_dump($_FILES);
 </head>
 
 <body>
-    <div class="container backgroundForm">
+    <div class="container">
         <div class="row">
-            <div class="col-sm mb-3">
+            <div class="col-sm-6 mb-3">
                 <div class="h2 mainTitle">Module d'Enregistrement d'Images</div>
-                <div class="h5 text-infoi">Mise en pratique PHP : Upload d'images.</div>
+                <div class="h5 text-info">Mise en pratique PHP : Upload d'images.</div>
             </div>
         </div>
         <div class="row">
@@ -131,7 +130,7 @@ var_dump($_FILES);
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-    <script src="assets/uploadPreview.js"></script>
+    <script src="assets\uploadPreview.js"></script>
 </body>
 
 </html>
