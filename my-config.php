278072106caa432aa051be8df90034cbce4cb28f
Sulian-Gouard <suliangouardlanoe@gmail.com>
2020-06-26 15:50:49 +0200

    to pull

--- my-config.php
+++ my-config.php
@@ -8,26 +8,15 @@ $usersArray = [
 
 $error = array();
 
-// if (!empty($_POST['login']) && (!empty($_POST['password']))) {
-//     if (!array_key_exists($_POST['login'], $usersArray)) {
-//         $error['login'] = 'login ou password invalide';
-//     }
-//     elseif ($usersArray[$_POST['login']] == $_POST['password']) {
-//         $_SESSION['login'] = $_POST['login'];
-//         header("Location: dashboard.php");
-//     } else {
-//         $error['login'] = 'login ou password invalide';
-//     }
-// }
-
 if (!empty($_POST['login']) && (!empty($_POST['password']))) {
     if (!array_key_exists($_POST['login'], $usersArray)) {
-        header("Location: not-allowed.php");
-    } elseif ($usersArray[$_POST['login']] == $_POST['password']) {
+        $error['login'] = 'login ou password invalide';
+    }
+    elseif ($usersArray[$_POST['login']] == $_POST['password']) {
         $_SESSION['login'] = $_POST['login'];
         header("Location: dashboard.php");
     } else {
-        header("Location: not-allowed.php");
+        $error['login'] = 'login ou password invalide';
     }
 }
 
