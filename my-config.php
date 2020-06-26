59fb50a2381717a50132a77490d332fa67767522
unknown <quentin-poirier@live.fr>
2020-06-26 15:47:58 +0200

    annulation header not-allowed et centrage alert erreur index.php

--- my-config.php
+++ my-config.php
@@ -1,5 +1,5 @@
 <?php
- session_start();
+session_start();
 
 $usersArray = [
     'admin' => 'navet',
@@ -8,25 +8,13 @@ $usersArray = [
 
 $error = array();
 
-// if (!empty($_POST['login']) && (!empty($_POST['password']))) {
-//     if (!array_key_exists($_POST['login'], $usersArray)) {
-//         $error['login'] = 'login ou password invalide';
-//     }
-//     elseif ($usersArray[$_POST['login']] == $_POST['password']) {
-//         $_SESSION['login'] = $_POST['login'];
-//         header("Location: dashboard.php");
-//     } else {
-//         $error['login'] = 'login ou password invalide';
-//     }
-// }
-
 if (!empty($_POST['login']) && (!empty($_POST['password']))) {
     if (!array_key_exists($_POST['login'], $usersArray)) {
-        header("Location: not-allowed.php");
+        $error['login'] = 'login ou password invalide';
     } elseif ($usersArray[$_POST['login']] == $_POST['password']) {
         $_SESSION['login'] = $_POST['login'];
         header("Location: dashboard.php");
     } else {
-        header("Location: not-allowed.php");
+        $error['login'] = 'login ou password invalide';
     }
 }
